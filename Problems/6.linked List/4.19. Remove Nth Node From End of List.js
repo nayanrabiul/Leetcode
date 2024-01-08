0f165099610ede2b53fd0b38a55efb0a93c5d334
nayanrabiul <nayan.rabiul@gmail.com>
2024-01-08 15:27:31 +0600

    Add sliding window approach for removing Nth node from end of linked list

---
+++ Problems/6.linked List/4.19. Remove Nth Node From End of List.js
@@ -0,0 +1,3 @@
+//2n complexity [ 2 interations ]
+// O(n) memory complexity [ 1 interation ]
+// sliding window
---
+++ Problems/6.linked List/5.141. Linked List Cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (!head) return false;
+
+    let fast = head;
+    let last = head;
+
+    while (fast) {
+        last = last.next;
+        fast = fast.next?.next;
+        if (fast === last) return true;
+    }
+    return false;
+};
+
+let data = {
+    val: 3,
+    next: null,
+};
+console.log(hasCycle(data));
