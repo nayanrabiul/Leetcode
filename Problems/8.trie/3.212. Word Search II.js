c5f81a095d5908d8cbe88629502627ed27f9573e
nayanrabiul <nayan.rabiul@gmail.com>
2024-02-27 21:44:42 +0600

    save

--- Problems/8.trie/3.212. Word Search II.js
+++ Problems/8.trie/3.212. Word Search II.js
@@ -6,11 +6,17 @@
 
 var findWords = function (board, words) {
     let trie = {};
+    let result = [];
     let row = board.length;
     let col = board[0].length;
     let visited = new Array(row).fill(0).map(() => new Array(col).fill(false));
 
-    let directions = [[0, 1], [0, -1][(1, 0)], [-1, 0]];
+    let directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ];
 
     let buildTrie = (words) => {
         for (let word of words) {
@@ -25,12 +31,24 @@ var findWords = function (board, words) {
         }
     };
 
-    let search = (i, j) => {
-        if (i < 0 || i >= row || j < 0 || j >= col) return;
+    let search = (i, j, node) => {
+        if (node.word) {
+            result.push(node.word);
+            node.word = null;
+        }
+        if (
+            i < 0 ||
+            i >= row ||
+            j < 0 ||
+            j >= col ||
+            visited[i][j] ||
+            !node[board[i][j]]
+        )
+            return;
 
         visited[i][j] = true;
         for (let [x, y] of directions) {
-            search(i + x, j + y);
+            search(i + x, j + y, node[board[i][j]]);
         }
         visited[i][j] = false;
     };
@@ -39,9 +57,10 @@ var findWords = function (board, words) {
 
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            search(i, j);
+            search(i, j, trie);
         }
     }
+    return result;
 };
 
 let board = [
